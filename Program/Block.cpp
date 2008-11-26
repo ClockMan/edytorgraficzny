0e3a306ea16c1775cf0585f94a1143b40dc246f1
piotrzsl <piotrzsl@fdc387ec-a432-11dd-9b86-41075523de00>
2008-11-26 10:22:34 +0000

    Wymaga poprawki, zerknij na to

--- Program/Block.cpp
+++ Program/Block.cpp
@@ -1,14 +1,28 @@
-#include <string>
-#include <vector>
-#include <exception>
-using namespace std;
-
 #include "Block.h"
-#include "BlockConfig.h"
-#include "BlockInput.h"
-#include "BlockOutput.h"
 
-PIWO::BlockConfig* PIWO::Block::getConfig() {
-	throw "Not yet implemented";
+Block::Block()
+{
+	config=new BlockConfig(copy.config);
 }
 
+Block::Block(Block &copy)
+{
+	config=new BlockConfig(copy.config);
+	//@TODO do napisania kopiowanie listy wejsc i wyjsc
+}
+
+Block::~Block()
+{
+    delete config;
+}
+
+BlockConfig* Block::getConfig()
+{
+	return config;
+}
+
+void Block::setConfig(BlockConfig &ob)
+{
+	delete config;
+	config=new BlockConfig(ob);
+}
