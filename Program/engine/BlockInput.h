0d11e4580a787c6bc1f8d942268d25711f0fc04a
PiotrWilk86 <PiotrWilk86@fdc387ec-a432-11dd-9b86-41075523de00>
2008-11-26 22:03:36 +0000

    Komentarze w kodzie.

--- Program/engine/BlockInput.h
+++ Program/engine/BlockInput.h
@@ -7,19 +7,48 @@ using namespace std;
 #define __BlockInput_h__
 
 #include "BlockElement.h"
-
+/**
+* BlockInput - Klasa pojemnik przechowuj¹ca wejœcia i wyjscia bloku
+* @author Piotr
+* @date 2008.11.25
+* @version 0.1
+*/
 class BlockInput: public BlockElement
 {
 		public:
 				AnsiString inputType;
 		public:
+		/**
+		* Konstruktor
+		* @param aName Nazwa wejœcia bloku
+		*/
 				BlockInput(const AnsiString aName);
+		/**
+		* Konstruktor kopiuj¹cy
+		*/
 				BlockInput(BlockInput &kopia);
+		 /**
+		 * Destruktor
+		 */
 				~BlockInput();
-
+		 /**
+		 * lista dozwolonych typów które mog¹ zostaæ podane na wejœcie
+		 */
 				vector<AnsiString> allowedTypes;
+		 /**
+		 * Realizujê po³¹czenia miêdzy blokami.
+		 * @param aName nazwa obiektu z którym bedziê po³¹czone wejœcie.
+		 * @return bool poprawnoœæ przeprowadzonej operajci.
+		 */
 				bool connect(const AnsiString aName);
+		 /**
+		 * Pobiera typ po³¹czenia.
+		 * @return nazwa typu po³¹czenia.
+		 */
 				AnsiString& getConnectedType();
+		  /**
+		  * roz³¹cza bloki
+		  */
 				void disconnect();
 };
 
--- Program/engine/BlockOutput.h
+++ Program/engine/BlockOutput.h
@@ -7,17 +7,41 @@ using namespace std;
 #define __BlockOutput_h__
 
 #include "BlockElement.h"
-
+/**
+* BlockInput - Klasa pojemnik przechowuj¹ca wejœcia i wyjscia bloku
+* @author Piotr
+* @date 2008.11.25
+* @version 0.1
+*/
 class BlockOutput: public BlockElement
 {
 		private:
 				AnsiString outputType;
 		public:
+			/**
+			* Konstruktor
+			* @param aName nazwa wyjœcia.
+			*/
 				BlockOutput(const AnsiString aName);
-				BlockOutput(BlockOutput &kopia);
+			/**
+			* Konstruktor kopiuj¹cy.
+			* @param copy obiekt który zostanie skopiowany
+			*/
+				BlockOutput(BlockOutput &copy);
+			/**
+			* Destruktor
+			*/
 				~BlockOutput();
-
+			/**
+			* Zwraca typ wyjœcia.
+			* @return AnsiString typ wyjœcia
+			*/
 				AnsiString& getOutputType();
+			/**
+			* Ustawia typ wyjœcia.
+			* @param aName typ wyjœcia.
+			* @return poprawnoœæ wykonanej operacji
+			*/
 				bool setOutputType(AnsiString aName);
 };
 
