b7cc6dddc064f9773a7ef2b979c583c9c04d62e4
piotrzsl <piotrzsl@fdc387ec-a432-11dd-9b86-41075523de00>
2008-12-05 17:05:38 +0000

    Rozbudowano przykład

--- Bloczki/Przykład/DLL.cpp
+++ Bloczki/Przykład/DLL.cpp
@@ -36,6 +36,7 @@ extern "C" __declspec(dllexport) int validate(Block *aBlock);
 bool showConfig(TComponent *owner, Block *aBlock)
 {
    TForm1 *x=new TForm1(owner);
+   x->block=aBlock;
    x->ShowModal();
    //odpalamy okienko konfiguracyjne bloczka, konfiguracja powinna byc nieuzale¿niona od wejœæ, mo¿e mieæ wp³yw na wyjœcia, po wykonaniu tej funkcji automatycznie zostanie wywo³ana funkcja validate.
    //wszystkie dane jakie chcemy przechowaæ zapisujemy w aBlock->getConfig().
@@ -49,12 +50,67 @@ int validate(Block *aBlock)
 {
    //funkcja sprawdza poprawnoœæ po³¹czeñ z bloczkiem jak i konfiguracji. tzn
    //dodawaæ nowe wejscia, dodawañ nowe wyjœcia, modyfikowaæ ich stany, kody b³êdów, ..
+   if (aBlock.input.size()==0)
+   {
+	  BlockInput x1("wejscie1");
+	  x1.allowedTypes.push_back("Bitmap16bit");
+	  x1.allowedTypes.push_back("Bitmap24bit");   	
+	  x1.setDescription("Hujowe wejscie 1");
+	  x1.setErrorCode(1);
+	  aBlock.input.push_back(x1);
+	  
+	  BlockInput x2("wejscie2");
+	  x2.allowedTypes.push_back("Bitmap16bit");
+	  x2.allowedTypes.push_back("Bitmap24bit");   	
+	  x2.setDescription("Hujowe wejscie 2");
+	  x2.setErrorCode(1);
+	  aBlock.input.push_back(x2);
+	  
+	  BlockOutput x("wyjscie");
+	  x.setOutputType("Btmap16bit");
+	  x.setDescription("Hujowe wyjscie");
+	  x.setErrorCode(2);
+	  aBlock.output.push_back(x);
+   }	
+   else
+   {
+   	 if (aBlock.input[0].getConnectionType().isEmpty())
+   	    aBlock.input[0].setErrorCode(1);
+   	    
+   	 if (aBlock.input[1].getConnectionType().isEmpty())
+   	    aBlock.input[1].setErrorCode(1);
+   	    
+   	 if ((aBlock.input[0].getConnectionType().isEmpty())||  (aBlock.input[1].getConnectionType().isEmpty()))
+   	  aBlock.output[0].setErrorCode(2);
+   	  
+   	 if ((aBlock.input[0].getConnectionType()=="Bitmap16bit")||  (aBlock.input[1].getConnectionType()=="Bitmap16bit"))
+   	  aBlock.output[0].setOutputType("Btmap16bit");
+   	  else
+   	  aBlock.output[0].setOutputType("Btmap24bit");
+	}
+
+   
    return 0;
 }
 
 int run(Block *aBlock)
 {
 	//funkcja wykonuje jak¹œ operacjê, najlepiej kod operacji przechowywaæ w zewnêtrznej bibliotece.
+	if (aBlock.input.Size()!=2) return 2;
+	if (aBlock.input[0].getConnectedType().isEmpty()) return 3;
+	if (aBlock.input[1].getConnectedType().isEmpty()) return 3;
+	
+	int c=aBlock.getConfig()->getInt("string1");
+	
+	TBitmap x1=IBitmap16bit::getBitmap(aBlock.input[0]->getObject());
+	TBitmap x2=IBitmap16bit::getBitmap(aBlock.input[1]->getObject());
+	
+	TBitmap x;
+	//dodawnaie bitmapy x1+x2==x
+	
+	TypeConfig *kopia=IBitmap16bit::getNew();
+	IBitmap16bit::setBitmap(kopia, x);
+	aBlock.ouput[0].setOutput(*kopia);
 	return 0;
 }
 
