048a2ad8be6e7e610e5e0dc27dcee033f348c91d
piotrzsl <piotrzsl@fdc387ec-a432-11dd-9b86-41075523de00>
2008-12-14 11:29:00 +0000

    Powstarją zalążki D:

--- Program/main.h
+++ Program/main.h
@@ -8,12 +8,42 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <Buttons.hpp>
+#include <ExtCtrls.hpp>
+#include "gui/PIWOEngine.h"
+#include <Menus.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
-private:	// User declarations
-public:		// User declarations
+	TMainMenu *MainMenu1;
+	TMenuItem *Plik1;
+	TMenuItem *Edycja1;
+	TMenuItem *Uruchom1;
+	TMenuItem *Nowy1;
+	TMenuItem *Otwrz1;
+	TMenuItem *Zapisz1;
+	TMenuItem *Zapiszjako1;
+	TMenuItem *Exportujjakoobraz1;
+	TMenuItem *Zamknij1;
+	TMenuItem *Zakocz1;
+	TMenuItem *Zaznaczwszystkiebloki1;
+	TMenuItem *Odznaczwszystkiebloki1;
+	TMenuItem *Odwrzaznaczenieblokw1;
+	TMenuItem *Odznaczpoczenie1;
+	TMenuItem *Duplikujbloki1;
+	TMenuItem *Usubloki1;
+	TMenuItem *Usupoczenie1;
+	void __fastcall FormCreate(TObject *Sender);
+
+private:
+	PIWOEngine *piwo;
+	int SearchDirectory(std::vector<AnsiString> &refvecFiles,
+					const AnsiString        &refcstrRootDirectory,
+					const AnsiString        &refcstrExtension,
+					bool                     bSearchSubdirectories = true);
+public:
+    PluginContener plugins;
 	__fastcall TForm1(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
--- Program/splash.h
+++ Program/splash.h
@@ -8,10 +8,16 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+#include <ComCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForm2 : public TForm
 {
 __published:	// IDE-managed Components
+	TImage *Image1;
+	TLabel *log;
+	TProgressBar *ProgressBar1;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm2(TComponent* Owner);
