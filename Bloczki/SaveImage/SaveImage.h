b795a0a618ce166f66f796067e31a245564e5649
banita21 <banita21@fdc387ec-a432-11dd-9b86-41075523de00>
2008-12-23 20:04:44 +0000

    poprawione otwieranie obrazu(byl wyciek pamieci) dodany bloczek zapisu obrazu w dowolnym bitrate. bloczek zapisuje jako bmp ale funkcja umozliwia zapis jako jpg(z zadana jakoscia) i tiff(z zadanym algorytmem kompresji)

---
+++ Bloczki/SaveImage/SaveImage.h
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+
+#ifndef SaveImageH
+#define SaveImageH
+
+#include <Graphics.hpp>
+#include <FreeImage.h>
+
+#include "../../Program/engine/Block.h"
+
+extern "C" __stdcall __declspec(dllexport) int run(Block *aBlock);
+extern "C" __stdcall __declspec(dllexport) bool showConfig(TComponent *owner, Block *aBlock);
+extern "C" __stdcall __declspec(dllexport) int validate(Block *aBlock);
+
+
+//---------------------------------------------------------------------------
+typedef DLL_API FIBITMAP* DLL_CALLCONV (*ConvertType)(FIBITMAP *dib);
+enum TYPE {bit_1, bit_4, bit_8, bit_16, bit_24, bit_32};
+//---------------------------------------------------------------------------
+
+/* ONLY FOR JPEG IMAGE !!! */
+
+// flags parameter can be :
+// JPEG_DEFAULT - Saves with good quality (75:1)
+// JPEG_QUALITYBAD - Saves with superb quality (100:1)
+// JPEG_QUALITYAVERAGE - Saves with good quality (75:1)
+// JPEG_QUALITYNORMAL - Saves with normal quality (50:1)
+// JPEG_QUALITYGOOD - Saves with average quality (25:1)
+// JPEG_QUALITYSUPERB - Saves with bad quality (10:1)
+// Integer X in [0..100] - Save with quality X:1
+// JPEG_PROGRESSIVE - Saves as a progressive JPEG file (use | to combine with JPEG quality flags)
+
+/* ONLY FOR TIFF IMAGE !!! */
+
+// flags parameter can be :
+// TIFF_DEFAULT - Save using CCITTFAX4 compression for 1-bit bitmaps and LZW compression for any other bitmaps
+// TIFF_CMYK - Stores tags for separated CMYK (use | to combine with TIFF compression flags)
+// TIFF_PACKBITS - Save using PACKBITS compression
+// TIFF_DEFLATE - Save using DEFLATE compression (also known as ZLIB compression)
+// TIFF_ADOBE_DEFLATE - Save using ADOBE DEFLATE compression
+// TIFF_NONE - Save without any compression
+// TIFF_CCITTFAX3 - Save using CCITT Group 3 fax encoding
+// TIFF_CCITTFAX4 - Save using CCITT Group 4 fax encoding
+// TIFF_LZW - Save using LZW compression
+// TIFF_JPEG - Save using JPEG compression (8-bit greyscale and 24-bit only. Default to LZW for other bitdepths)
+
+bool SaveImage(Graphics::TBitmap* picture, AnsiString path, TYPE bit_per_pixel, int flags = 0);
+//---------------------------------------------------------------------------
+bool WriteImage(FIBITMAP* image, Graphics::TBitmap* picture);
+FIBITMAP* BitrateConversion(FIBITMAP* image, TYPE bit_per_pixel);
+//---------------------------------------------------------------------------
+#endif
---
+++ Bloczki/SaveImage/configWindow.cpp
@@ -0,0 +1,70 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "configWindow.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TcfgWindow *cfgWindow;
+//---------------------------------------------------------------------------
+__fastcall TcfgWindow::TcfgWindow(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TcfgWindow::FormClose(TObject *Sender,
+      TCloseAction &Action)
+{
+	cfg_ = NULL;
+	Action = caFree;
+}
+//---------------------------------------------------------------------------
+void __fastcall TcfgWindow::SetConfig(Block* block)
+{
+ 	cfg_ = block->getConfig();
+
+	if(cfg_->getString("bitrate") == "Bitmap24bit")
+		bit_24->Checked = true;
+	else if(cfg_->getString("bitrate") == "Bitmap32bit")
+		bit_32->Checked = true;
+	else if(cfg_->getString("bitrate") == "Bitmap16bit")
+		bit_16->Checked = true;
+	else if(cfg_->getString("bitrate") == "Bitmap8bit")
+		bit_8->Checked = true;
+	else if(cfg_->getString("bitrate") == "Bitmap4bit")
+		bit_4->Checked = true;
+	else
+		bit_1->Checked = true;
+	
+	EditPath->Text = cfg_->getString("path");
+}
+//---------------------------------------------------------------------------
+void __fastcall TcfgWindow::CancelClick(TObject *Sender)
+{
+	Close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TcfgWindow::Button1Click(TObject *Sender)
+{
+	if(SaveDialog1->Execute())
+		EditPath->Text = SaveDialog1->FileName;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TcfgWindow::OKClick(TObject *Sender)
+{
+	cfg_->setString("path",EditPath->Text);
+	if(bit_24->Checked) cfg_->setString("bitrate","Bitmap24bit");
+	else if(bit_32->Checked) cfg_->setString("bitrate","Bitmap32bit");
+	else if(bit_16->Checked) cfg_->setString("bitrate","Bitmap16bit");
+	else if(bit_8->Checked) cfg_->setString("bitrate","Bitmap8bit");
+	else if(bit_4->Checked) cfg_->setString("bitrate","Bitmap4bit");
+	else cfg_->setString("bitrate","Bitmap1bit");
+
+	Close();	
+}
+//---------------------------------------------------------------------------
+
