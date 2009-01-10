936a114c98e8521144de33693fda396ba775c5fb
slawko.u <slawko.u@fdc387ec-a432-11dd-9b86-41075523de00>
2009-01-10 22:54:26 +0000

    zmieniono algorytm odejmowania z algorytmu ze skalowaniem do przedziału <0,255> na algorytm bez skalowania i z zabronionym wyjściem poza przedział <0,255>

--- trunk/Bloczki/Subtraction-zatwierdzono/subtraction.cpp
+++ trunk/Bloczki/Subtraction-zatwierdzono/subtraction.cpp
@@ -18,7 +18,7 @@ bool Subtraction(Graphics::TBitmap* picture1, Graphics::TBitmap* picture2)
 	picture2->PixelFormat = pf32bit;
 	RGBQUAD *row1;
 	RGBQUAD *row2;
-
+	int result;
 	for(int i(0); i < picture1->Height && i < picture2->Height; ++i)
 	{
 		row1 = (RGBQUAD *) picture1->ScanLine[i];
@@ -28,9 +28,24 @@ bool Subtraction(Graphics::TBitmap* picture1, Graphics::TBitmap* picture2)
 
 		for (int j(0); j < picture1->Width && j < picture2->Width; ++j)
 		{
+			// wersja z zabronionym wyjœciem poza zakres <0,255>
+			result = (int)row1[j].rgbRed-(int)row2[j].rgbRed;
+			if(result < 0) result = 0;
+			row1[j].rgbRed = (BYTE)result;
+
+			result = (int)row1[j].rgbGreen-(int)row2[j].rgbGreen;
+			if(result < 0) result = 0;
+			row1[j].rgbGreen = (BYTE)result;
+
+			result = (int)row1[j].rgbBlue-(int)row2[j].rgbBlue;
+			if(result < 0) result = 0;
+			row1[j].rgbBlue = (BYTE)result;
+
+			/* wersja ze skalowaniem
 			row1[j].rgbRed = (BYTE)((((float)row1[j].rgbRed-(float)row2[j].rgbRed))/2.+128);
 			row1[j].rgbGreen = (BYTE)((((float)row1[j].rgbGreen-(float)row2[j].rgbGreen))/2.+128);
 			row1[j].rgbBlue = (BYTE)((((float)row1[j].rgbBlue-(float)row2[j].rgbBlue))/2.+128);
+			*/
 		}
 	}
 
